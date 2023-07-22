2dd2cb598dd6d37cb9035dbd8bb75ec07b9c2c4f
Luis Guillermo Luque Condori <lluquecon@unsa.edu.pe>
2023-07-21 21:19:16 -0500

    Parentesis

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 let currentValue = "";
 
+function updateOpacity() {
+    const inputElement = document.getElementById("input");
+    const resultElement = document.getElementById("result");
+    if (inputElement.textContent.trim() === "Ingrese una expresion...") {
+        inputElement.classList.add("initial-value");
+    } else {
+        inputElement.classList.remove("initial-value");
+    }
+    if (resultElement.textContent.trim() === "Resultado") {
+        resultElement.classList.add("initial-value");
+    } else {
+        resultElement.classList.remove("initial-value");
+    }
+}
+
 function addValue(value) {
     currentValue += value;
     document.getElementById("input").textContent = currentValue;
     updateResult();
+    updateOpacity();
 }
 
 function addOperator(operator) {
@@ -21,6 +37,7 @@ function calculate() {
             result = parseFloat(result.toFixed(4))
             document.getElementById("result").innerHTML = result;
             currentValue = result.toString();
+            updateOpacity();
         } catch (error) {
             document.getElementById("result").innerHTML = "Error";
         }
@@ -31,6 +48,7 @@ function clearResult() {
     document.getElementById("input").innerHTML = "Ingrese una expresion...";
     document.getElementById("result").innerHTML = "Resultado";
     currentValue = "";
+    updateOpacity();
 }
 
 function updateResult() {
